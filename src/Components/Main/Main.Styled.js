02be8172e1d8dba993abb054bb01214843f8104d
ayearsrain <ayearsrain@gmail.com>
2022-09-04 20:57:57 +0100

    added dynamic midi messages

--- src/Components/Main/Main.Styled.js
+++ src/Components/Main/Main.Styled.js
@@ -1,5 +1,5 @@
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.main`
 
@@ -18,3 +18,43 @@ export const Title = styled.h1`
   margin: 0 0 2rem 0;
 
 `;
+
+export const Form = styled.form`
+
+  max-width: 40rem;
+
+  padding: 1rem;
+  margin: 1rem auto;
+
+`;
+
+export const Label = styled.label`
+
+  display: block;
+  margin-bottom: 0.5rem;
+
+`;
+
+const FormItem = css`
+
+  display: block;
+  width: 100%;
+
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid #ddd;
+
+`;
+
+export const Select = styled.select`
+
+  ${FormItem}
+
+`;
+
+export const Input = styled.input`
+
+  ${FormItem}
+
+`;
+
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,13 +2,18 @@
 import React, { useEffect, useState } from 'react';
 import { WebMidi } from 'webmidi';
 
-import { Container, Title } from './Main.Styled';
+import { Container, Title, Form, Label, Select, Input } from './Main.Styled';
 import Pedal from '../Pedal/Pedal';
 
 const Main = () => {
 
   const [midiOutputs, setMidiOutputs] = useState();
   const [selectedMidiOutput, setSelectedMidiOutput] = useState();
+
+  const [midiCcMessageA, setMidiCcMessageA] = useState( 0 );
+  const [midiCcValueA, setMidiCcValueA] = useState( 0 );
+  const [midiCcMessageB, setMidiCcMessageB] = useState( 0 );
+  const [midiCcValueB, setMidiCcValueB] = useState( 0 );
 
   const handleMidiSelectChange = ( event ) => {
     
@@ -20,9 +25,15 @@ const Main = () => {
 
     if( selectedMidiOutput !== null ) {
 
-      console.log( 'midi not null', selectedMidiOutput );
+      if( value === 0 ) {
 
-      selectedMidiOutput.sendControlChange( 41, value + 1 );
+        selectedMidiOutput.sendControlChange( midiCcMessageA, midiCcValueA );
+
+      } else {
+
+        selectedMidiOutput.sendControlChange( midiCcMessageB, midiCcValueB );
+
+      }
 
     }
 
@@ -43,19 +54,41 @@ const Main = () => {
 
       <Title>Expression Flipper</Title>
 
-      <select defaultValue={-1} onChange={handleMidiSelectChange}>
-
-        <option value={-1} disabled={true}>Select a MIDI Output</option>
-
-        {midiOutputs?.map( ( item, key ) => (
-
-          <option value={item.id} key={key}>{item.name}</option>
-
-        ))}
-
-      </select>
-
       <Pedal onChange={handlePedalValueChange} />
+
+      <Form>
+
+        <Label htmlFor="midi-output-select">MIDI Output Device:</Label>
+
+        <Select id="midi-output-select" defaultValue={-1} onChange={handleMidiSelectChange}>
+
+          <option value={-1} disabled={true}>Select a MIDI Output Device</option>
+
+          {midiOutputs?.map( ( item, key ) => (
+
+            <option value={item.id} key={key}>{item.name}</option>
+
+          ))}
+
+        </Select>
+
+        <Label>MIDI CC Message A:</Label>
+
+        <Input value={midiCcMessageA} onChange={event => setMidiCcMessageA( event.target.value )} />
+
+        <Label>MIDI CC Value A:</Label>
+
+        <Input value={midiCcValueA} onChange={event => setMidiCcValueA( event.target.value )} />
+
+        <Label>MIDI CC Message B:</Label>
+
+        <Input value={midiCcMessageB} onChange={event => setMidiCcMessageB( event.target.value )} />
+
+        <Label>MIDI CC Value B:</Label>
+
+        <Input value={midiCcValueB} onChange={event => setMidiCcValueB( event.target.value )} />
+
+      </Form>
 
     </Container>
 
